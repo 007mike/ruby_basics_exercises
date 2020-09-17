ba19878d1bf14093ac1a10d9dcce8dbe69ce8b94
Michael Lawrence <007mike@gmail.com>
2020-09-17 16:28:45 -0400

    adding debug exercises

---
+++ debugging/digit_product.rb
@@ -0,0 +1,15 @@
+# digit_product.rb
+
+def digit_product(str_num)
+  digits = str_num.chars.map { |n| n.to_i }
+  product = 1
+
+  digits.each do |digit|
+    product *= digit
+  end
+
+  product
+end
+
+
+p digit_product('12345')
